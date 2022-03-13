066102b17bb1477c71ee9c6a60a60b915fdce5a8
rmd3 <michaeldanjolell@gmail.com>
2022-03-13 18:36:43 -0400

    Add project files.

---
+++ D'Anjolell_13_1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace D_Anjolell_13_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBxResults = new System.Windows.Forms.ListBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstBxResults
+            // 
+            this.lstBxResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lstBxResults.FormattingEnabled = true;
+            this.lstBxResults.ItemHeight = 31;
+            this.lstBxResults.Location = new System.Drawing.Point(225, 22);
+            this.lstBxResults.Name = "lstBxResults";
+            this.lstBxResults.Size = new System.Drawing.Size(331, 314);
+            this.lstBxResults.TabIndex = 0;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnFind.Location = new System.Drawing.Point(429, 351);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(127, 49);
+            this.btnFind.TabIndex = 1;
+            this.btnFind.Text = "Find all";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnRemove.Location = new System.Drawing.Point(225, 351);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(179, 49);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove all";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.lstBxResults);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBxResults;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ D'Anjolell_13_1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_Anjolell_13_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            //Richard D'Anjolell
+            //Declare Variables
+            var stringFile = File.ReadAllLines("..\\..\\random.txt");
+            var stringList = new List<string>(stringFile);
+            List<int> numList = new List<int>();
+
+            // Create a Predicate to determine whether a number is base (between 1 and 10)
+            Predicate<int> IsBase = delegate (int x)
+            {
+                bool isBaseNumber = false;
+
+                if (x >= 1 && x <= 10)
+                {
+                    isBaseNumber = true;
+                }
+
+                return isBaseNumber;
+            };
+
+            //Turns string list into int list
+            foreach (string i in stringList)
+            {
+                numList.Add(int.Parse(i));
+            }
+
+            // Find all of the base numbers in the list
+            List<int> baseNum = numList.FindAll(IsBase);
+
+            //Clears list box each time before filling it up
+            lstBxResults.Items.Clear();
+            // Display the base numbers in list box
+            foreach (int i in baseNum)
+            {
+                lstBxResults.Items.Add(i);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            //Declare Variables
+            var stringFile = File.ReadAllLines("..\\..\\random.txt");
+            var stringList = new List<string>(stringFile);
+            List<int> numList = new List<int>();
+
+            //Turns string list into int list
+            foreach (string i in stringList)
+            {
+                numList.Add(int.Parse(i));
+            }
+
+            // Remove all the elements containing a negative value.
+            numList.RemoveAll(x => x < 0);
+
+            //Clears list box each time before filling it up
+            lstBxResults.Items.Clear();
+            // Display positive numbers in list box
+            numList.ForEach(x => lstBxResults.Items.Add(x));
+        }
+    }
+}
